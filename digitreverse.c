d6502f65bfe7122ce8c8f685138a0b1d31230759
some-guy-you-know <72244083+some-guy-you-know@users.noreply.github.com>
2020-10-02 11:37:26 +0530

    update digrev.c
    documentation

--- digitreverse.c
+++ digitreverse.c
@@ -2,16 +2,21 @@
 int main()
 {
 	int n, no, s=0;
+	
+	//input for the operation
 	printf("Enter the number you want to reverse: ");
 	scanf("%d",&n);
-
+	
+	//for loop
 	for(;n>0;) {
 		no=n%10;
 		n=n/10;
 		s=s*10+no;
 	}
 
+	//printing the output number
 	printf("%d\n", s);
 	
 	return 0;
 }
+//program fininshes
